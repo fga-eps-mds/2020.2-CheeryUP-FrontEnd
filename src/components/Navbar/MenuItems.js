20c3329bd821512fbc760add341151132b85cf4e
juninhigh <nilvanperes@gmail.com>
2021-03-11 23:26:19 -0300

    feature_navBar_HomePage

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Serviços',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Loguin',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Cadastre-se',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
