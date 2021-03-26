ed28c4974fad2e4965bdbf7abde7f014c6df3d6c
fernandes-natanael <filhonatanael01@gmail.com>
2021-03-26 18:54:37 -0300

    feature_first forms

---
+++ src/components/Login/Login.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import useState from 'react'
+import './Login.css';
+import useForm from '../UserForm/userForm';
+
+
+function validate(values){
+    let errors = {};
+
+    if (!values.email) {
+        errors.email = 'É necessário preencher seu e-mail';
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+        errors.email = 'E-mail inválido. Preencha com algum e-mail válido';
+    }
+    if (!values.senha) {
+        errors.senha = 'É necessário preencher uma senha';
+    } else if (values.senha.length < 6) {
+        errors.senha = 'A senha deve conter mais de 6 caracteres';
+    }
+    return errors;
+};
+
+function SubmitForm(validate){
+    if (validate != 0){
+        document.write("Boaaaaaaa cuzao")
+    }
+}
+
+
+function FormSucess () {
+    return (
+        <div className = "form-sucess">
+            <h1> Recebemos o seu dados. YUUUUUUUUUUUUUUUUPI TÚ É DA EQUIPE GARAI</h1>
+        </div>
+    )    
+}
+
+
+const Login = ({ SubmitForm }) => {
+    const { handleChange, values, handleSubmit, errors } = useForm(SubmitForm, validate);
+
+
+
+    return (
+        <>
+            <div className="form-container">
+
+                <div className="form-content-center">
+                    <form className='form' onSubmit={handleSubmit}>
+                        
+                        <div className="form-inputs">
+                            <label htmlFor="email" className="form-label">Email:</label>
+                            <input
+                                id='email'
+                                type="email"
+                                name='email'
+                                className="form-input"
+                                placeholder="E-mail"
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                            {errors.email && <p>{errors.email}</p>}
+                        </div>
+
+                        
+                        <div className="form-inputs">
+                            <label htmlFor="senha" className="form-label">Senha:</label>
+                            <input
+                                id="senha"
+                                type="password"
+                                name='senha'
+                                className="form-input"
+                                placeholder="Senha"
+                                value={values.senha}
+                                onChange={handleChange}
+                            />
+                            {errors.senha && <p>{errors.senha}</p>}
+                        </div>
+
+                        
+                        <button className="form-input-btn" type="submit">Login</button>
+                        <span className="form-input-login">
+                            Não possui login ? 
+                            <a  href="#"> Cadastre-se </a>
+                        </span>
+
+                    </form>
+
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+   
+
+export default Login;
---
+++ src/components/UserForm/userForm.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import '../Login/Login'
+
+export default function useForm(callback, validate) {
+    const [values, setValues] = useState({
+        email: '',
+        senha: '',
+    });
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        setErrors(validate(values));
+
+        setIsSubmitting(true);
+    };
+
+    useEffect(
+        () => {
+            if (Object.keys(errors).length == 0 && isSubmitting) {
+                console.log(values);
+                alert("Cadastro efetuado passado!");          
+            }
+        },
+        [errors]
+    );
+
+
+    return { handleChange, values, handleSubmit, errors };
+};
+
+
+
