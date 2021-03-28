ae202e74c41a0286f4065f35474edc50c9da110a
antoniotoineto <netocaastro10@gmail.com>
2021-03-28 00:21:26 -0300

    feature_Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,14 @@ import Navbar from "../components/Navbar/Navbar";
 import Body from '../components/Body/Body';
 import About from '../components/About/About';
 //import '../styles/Home';
-import {BrowserRouter as Router} from 'react-router-dom';
+//import {BrowserRouter as Router} from 'react-router-dom';
 
 function Home() {
   return(
     <div className='Homepage'>
-      <Router>
         <Navbar />
         <Body />
         <About />
-      </Router>
     </div>
   ) 
 }
