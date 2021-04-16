15f9f5a4305af2c59db5dc26b84b758b06d69c37
fernandes-natanael <filhonatanael01@gmail.com>
2021-04-15 23:49:43 -0300

    melhora na responsividade da imagem lateral

--- src/pages/Registrar/SignUP.js
+++ src/pages/Registrar/SignUP.js
@@ -42,51 +42,51 @@ const SignUP = ({ SubmitForm }) =>
         <Fragment>
             
             <div className="container">
-            <div className="form-signup-container">
-                <div className="top-signup">
-                    <a href="http://localhost:3000/"> 
-                        <img className="top-signup-logo" src="img/logo_word.png" alt="logo CheeryUp" />
-                    </a>
+                <div className="form-signup-container">
+                    <div className="top-signup">
+                        <a href="http://localhost:3000/"> 
+                            <img className="top-signup-logo" src="img/logo_word.png" alt="logo CheeryUp" />
+                        </a>
 
-                    <a href="http://localhost:3000/"> 
-                        <label className="top-signup-navbar-option">Início</label>
-                    </a>
-                </div>
-                
-                <div className="form-signup-wrap">
-                    <Card className="form-card" style={{ border: "none", boxShadow: "none" }}>
-                        <Card.Content className="form-title"> 
-                            <Card.Header> Cadastrar </Card.Header>
-                        </Card.Content>
-                        <Card.Content> 
-                            <Form className="form-content-input">
-                                <Form.Input placeholder="Digite seu nome" label="Nome" required fluid onChange={handleChange} name='nome' value={values.nome}  /> 
-                                    {errors.nome && <p class="p-message">{errors.nome}</p>}
-                                <Form.Input placeholder="Digite seu e-mail" label="Email" required fluid onChange={handleChange} name='email' value={values.email} /> 
-                                    {errors.email && <p class="p-message">{errors.email}</p>}
-                                <Form.Input placeholder="Digite o número do CRP" label="Número do CRP" required fluid onChange={handleChange} name='nCRP' value={values.nCRP} /> 
-                                    {errors.nCRP && <p class="p-message">{errors.nCRP}</p>}
-                                <Form.Input placeholder="Digite sua senha" label="Senha" type="password" required fluid onChange={handleChange} name='senha' value={values.senha} /> 
-                                    {errors.senha && <p class="p-message">{errors.senha}</p>}
-                                <Form.Input placeholder="Confirme sua senha" label="Confirmar senha" type="password" required fluid onChange={handleChange} name='senha2' value={values.senha2} /> 
-                                    {errors.senha2 && <p class="p-message">{errors.senha2}</p>}
+                        <a href="http://localhost:3000/"> 
+                            <label className="top-signup-navbar-option">Início</label>
+                        </a>
+                    </div>
+                    
+                    <div className="form-signup-wrap">
+                        <Card className="form-card" style={{ border: "none", boxShadow: "none" }}>
+                            <Card.Content className="form-title"> 
+                                <Card.Header> Cadastrar </Card.Header>
+                            </Card.Content>
+                            <Card.Content> 
+                                <Form className="form-content-input">
+                                    <Form.Input placeholder="Digite seu nome" label="Nome" required fluid onChange={handleChange} name='nome' value={values.nome}  /> 
+                                        {errors.nome && <p class="p-message">{errors.nome}</p>}
+                                    <Form.Input placeholder="Digite seu e-mail" label="Email" required fluid onChange={handleChange} name='email' value={values.email} /> 
+                                        {errors.email && <p class="p-message">{errors.email}</p>}
+                                    <Form.Input placeholder="Digite o número do CRP" label="Número do CRP" required fluid onChange={handleChange} name='nCRP' value={values.nCRP} /> 
+                                        {errors.nCRP && <p class="p-message">{errors.nCRP}</p>}
+                                    <Form.Input placeholder="Digite sua senha" label="Senha" type="password" required fluid onChange={handleChange} name='senha' value={values.senha} /> 
+                                        {errors.senha && <p class="p-message">{errors.senha}</p>}
+                                    <Form.Input placeholder="Confirme sua senha" label="Confirmar senha" type="password" required fluid onChange={handleChange} name='senha2' value={values.senha2} /> 
+                                        {errors.senha2 && <p class="p-message">{errors.senha2}</p>}
 
-                                <Button type="submit" onClick={handleSubmit}>Cadastrar </Button>
-                                <span className="form-input-loguin">
-                                    Já possui cadastro ?
-                                    <a  href="http://localhost:3000/Login">Faça Login. </a>
-                                </span>
-                            </Form>
-                        </Card.Content>
-                    </Card>
-                
-                </div>             
+                                    <Button type="submit" onClick={handleSubmit}>Cadastrar </Button>
+                                    <span className="form-input-loguin">
+                                        Já possui cadastro ?
+                                        <a  href="http://localhost:3000/Login">Faça Login. </a>
+                                    </span>
+                                </Form>
+                            </Card.Content>
+                        </Card>
+                    
+                    </div>             
             
-            </div>
+                </div>
 
-            <div className="side-image">
-                <img src="img/wallpaper_cadastro.png" alt="Wallpaper cadastro"/>
-            </div> 
+                <div className="side-image">
+                    <img src="img/wallpaper_cadastro.png" alt="Wallpaper cadastro"/>
+                </div> 
 
             </div>
 
