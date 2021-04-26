8a505fea477dc058b0231db05ff4d1f6adf551ab
juninhigh <nilvanperes@gmail.com>
2021-04-26 12:45:57 -0300

    refact_login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -25,46 +25,50 @@ const Login = ({ SubmitForm }) => {
 
     return (
             <Fragment>
+                <div className="container-login"> 
+                        <div className="form-login-container">
+                            <div className="top-login">
+                                <a href="/"> 
+                                    <img className="top-login-logo" src="img/logo_word.png" alt="logo CheeryUp" />
+                                </a>
+                                <a href="/"> 
+                                    <label className="top-login-navbar-option">Início</label>
+                                </a>
+                            </div>
 
-                <div className="form-login-container">
-                    <div class="top-login" >
-                        <img src="img/logo_word.png" alt="cereja" class="form-img-login" />
-                        <a href="/"> 
-                        <button className="form-input-btn-home" type="submit">Inicio</button>  
-                        </a>
-                    </div>
+                        <div className="form-login-wrap">
+                            <Card className = "form-card-login" style={{ border: "none", boxShadow: "none" }}>
 
-                    <div className="form-login-wrap">
-                        <Card className = "form-ui-card" style={{ border: "none", boxShadow: "none" }}>
-                        
-                            <Card.Content style={{ border: "none", boxShadow: "none" }}>
-                                <Image className="logo-img" src='/imagens/logo.png' alt='Logo-login' wrapped ui={false} />
-                            </Card.Content>
-
-                            <Card.Content className = "form-title-login" style={{ border: "none", boxShadow: "none" }}>
-                                <Card.Header> Login </Card.Header>
-                            </Card.Content>
+                                <Card.Content className = "form-title-login" style={{ border: "none", boxShadow: "none" }}>
+                                    <Card.Header> Login </Card.Header>
+                                </Card.Content>
+                                
+                                <Card.Content> 
+                                    <Form className ="form-content-input-login">
+                                        <Form.Input placeholder="Email" label="Email" required fluid onChange={handleChange} name='email' value={values.email}/>
+                                            {errors.email && <p class="p-message">{errors.email}</p>}
+                                        <Form.Input placeholder="Senha" label="Senha" required fluid onChange={handleChange} name='senha' value={values.senha}/>
+                                            {errors.senha && <p class="p-message">{errors.senha}</p>}
+                                            <p><a className="links" href="/RecuperarSenha">Esqueceu sua senha?</a></p>
+                                        <Button type="submit" onClick={handleSubmit}>Login</Button>
                             
-                            <Card.Content> 
-                                <Form className ="form-content-input-login">
-                                    <Form.Input placeholder="Email" label="Email" required fluid onChange={handleChange} name='email' value={values.email}/>
-                                        {errors.email && <p class="p-message">{errors.email}</p>}
-                                    <Form.Input placeholder="Senha" label="Senha" required fluid onChange={handleChange} name='senha' value={values.senha}/>
-                                        {errors.senha && <p class="p-message">{errors.senha}</p>}
-                                        <p><a className="links" href="http://localhost:3000/RecuperarSenha">Esqueceu sua senha?</a></p>
-                                    <Button type="submit" onClick={handleSubmit}>Login</Button>
-                           
-                                </Form>
-                            </Card.Content>
-                            <br />
-                            <span className="form-input-help">
-                                        Não possui uma conta ?
-                                        <a className="links" href="/SignUP">Cadastre-se</a>
-                            </span>
-                        </Card>
-                   
-                    </div>
+                                    </Form>
+                                </Card.Content>
+                                <br />
+                                <span className="form-input-help">
+                                            Não possui uma conta ?
+                                            <a className="links" href="/SignUP">Cadastre-se</a>
+                                </span>
+                            </Card>
+                    
+                        </div>
 
+                    </div>
+                
+                    <div className="side-image-login">
+                        <img src="img/wallpaper_login.png" alt="Wallpaper login"/>
+                    </div> 
+                
                 </div>
             </Fragment>
         
