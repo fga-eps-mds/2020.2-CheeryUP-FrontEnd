b7e12ac97550a96e356dc8e0698ce3ede8998ca5
Abraao Alves <abraao1231@gmail.com>
2021-04-28 19:35:08 -0300

    feat_#130/ implementação de grafico para regioes

--- src/pages/graficos/GraficoRegiao.js
+++ src/pages/graficos/GraficoRegiao.js
@@ -1,59 +1,178 @@
 import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import api from '../../services/api'
-import {Component} from 'react'
-
-
+import { Component } from 'react'
 
 
 class GraficoRegiao extends Component {
+    state = {
+        pessoas: [],
+    }
 
-  state = {
-    pessoas: [],
-  }
-  
-    async componentDidMount(){
+    async componentDidMount() {
         const response = await api.get('/psicologos/33333333333/pacientes');
         this.setState({ pessoas: response.data });
-      }
+    }
 
-render() {       
-    const { pessoas } = this.state;
-    
-    const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [10,10,10,10,10,10],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
-    return (
-      <>
-        <Doughnut data={data} />
-      </>)
-    
-  }
+    render() {
+        const { pessoas } = this.state;
+        var regioes = pessoas.map(pessoa => pessoa.regiao);
+        console.log("regioes Totais:", regioes);
 
-  
+        var legenda = [];
+        var qtdRegioes = [];
+
+        regioes.forEach(regiao => {
+            if (!legenda.includes(regiao)) {
+                legenda.push(regiao);
+                qtdRegioes.push(1);
+            }
+            else {
+                qtdRegioes[legenda.indexOf(regiao)] += 1;
+            }
+        });
+
+        legenda.forEach(regiao => {
+            switch (regiao) {
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+                    break;
+                case "AC":
+                    legenda[legenda.indexOf(regiao)] = "Águas Claras"
+            }
+        })
+
+        console.log(legenda);
+
+        const data = {
+            labels: legenda,
+            datasets: [
+                {
+                    label: 'Pessoas por região',
+                    data: qtdRegioes,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.7)',
+                        'rgba(54, 162, 235, 0.7)',
+                        'rgba(255, 206, 86, 0.7)',
+                        'rgba(75, 192, 192, 0.7)',
+                        'rgba(153, 102, 255, 0.7)',
+                        'rgba(255, 159, 64, 0.7)',
+                    ],
+
+                    borderWidth: 1,
+                },
+            ],
+        };
+        return (
+            <>
+                <Bar data={data} />
+            </>)
+
+    }
+
+
 }
 
 
