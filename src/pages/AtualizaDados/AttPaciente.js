f14a8e8e38224023d7161036ebdfe48815d80f49
antoniotoineto <netocaastro10@gmail.com>
2021-04-28 22:27:51 -0300

    feature/PagAttDadosPaciente #128

--- src/pages/AtualizaDados/AttPaciente.js
+++ src/pages/AtualizaDados/AttPaciente.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Button, Card, Form, Dropdown } from 'semantic-ui-react'
 import NavbarPsic from '../../components/Navbar/NavbarPsic';
-import '../../style/pages/Cadastro/CadastroPaciente.css';
+import '../../style/pages/AtualizaDados/AttPaciente.css';
 
 const opcoesRegiao = [
     { key: 1, text: 'Águas Claras', value: 1 },
@@ -15,7 +15,7 @@ const opcoesRegiao = [
     { key: 9, text: 'Guará', value: 9 },
   ]
 
-class CadastroPac extends Component {
+class AttDadosPac extends Component {
     state = {}
 
     handleChange = (e, { value }) => this.setState({ value })
@@ -23,39 +23,39 @@ class CadastroPac extends Component {
     render() {
         const { value } = this.state
         return (
-            <Fragment className="container-cadPac">
+            <Fragment className="container-attPac">
                 <NavbarPsic />
 
-                <div className="dados-psicologo">
+                <div className="dados-psicologo-attPac">
                     Nome do Psicólogo <br/>
                     Número do CRP
                 </div>
 
 
-                <div className="form-cadPac">
-                    <div className="titulo-form">
-                        <h1 className="titulo">Cadastro de Paciente</h1>
+                <div className="form-attPac">
+                    <div className="titulo-form-attPac">
+                        <h1 className="titulo-att">Atualizar dados - Paciente</h1>
                     </div>
                     <Form>
-                        <div className="form-items">
-                            <div className="primeira-linha">
+                        <div className="form-items-attPac">
+                            <div className="primeira-linha-att">
                                 <Form.Group widths='equal'>
-                                    <Form.Input fluid label="Nome" placeholder="Nome do Paciente" />
+                                    <Form.Input fluid label="Nome" placeholder="Paciente x" />
                                 </Form.Group>
                             </div>
-                            <div className="segunda-linha">
+                            <div className="segunda-linha-att">
                                 <Form.Group widths='equal'>
-                                    <Form.Input fluid label='Data de Nascimento' placeholder='dd/mm/aaaa' />
-                                    <Form.Input fluid label='CPF' placeholder='CPF do Paciente' />
+                                    <Form.Input fluid label='Data de Nascimento' placeholder='11/11/2001' />
+                                    <Form.Input fluid label='CPF' placeholder='12345678910' />
                                     <Form.Select
                                         fluids
                                         label='Região'
                                         options={opcoesRegiao}
-                                        placeholder='Região do Paciente'
+                                        placeholder='Local X'
                                     />
                                 </Form.Group>
                             </div>
-                            <div className="terceira-linha">
+                            <div className="terceira-linha-att">
                                 <Form.Group inline>
                                 <label>Gênero</label>
                                 <Form.Radio
@@ -78,11 +78,11 @@ class CadastroPac extends Component {
                                 />
                                 </Form.Group>
                             </div>
-                            <div className="quarta-linha">
-                                <Form.TextArea label='Descrição' placeholder='Informações adicionais do paciente...' />
+                            <div className="quarta-linha-att">
+                                <Form.TextArea label='Descrição' placeholder='Paciente apresenta um quadro de...' />
                             </div>
-                            <div className="quinta-linha">
-                                <Form.Button>Cadastrar</Form.Button>
+                            <div className="quinta-linha-att">
+                                <Form.Button>Atualizar</Form.Button>
                             </div>
                         </div>
                     </Form>
@@ -92,4 +92,4 @@ class CadastroPac extends Component {
     }
 }
 
-export default CadastroPac;
+export default AttDadosPac;
