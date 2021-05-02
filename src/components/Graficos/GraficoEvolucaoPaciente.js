e9e8efde84460170b0636ecf3fd603bb64d8fee4
Abraao Alves <abraao1231@gmail.com>
2021-05-02 16:52:29 -0300

    feat_add_grafico de evolucao #41 #143

--- src/components/Graficos/GraficoEvolucaoPaciente.js
+++ src/components/Graficos/GraficoEvolucaoPaciente.js
@@ -2,7 +2,6 @@ import { Line } from 'react-chartjs-2';
 import api from '../../services/api'
 import { Component } from 'react'
 
-
 class GraficoEvolucaoPaciente extends Component {
 
     state = {
@@ -17,35 +16,41 @@ class GraficoEvolucaoPaciente extends Component {
     render() {
 
         var {consultas} = this.state
-        var media = [];
+        var mediaConsulta = [];
+        mediaConsulta[0] = 0;
         var soma = 0;
+        var qualidadeDeVida = [];
        consultas.forEach(consulta => {
-        for (var indicador in consulta){        
-            if (indicador != "registro"){  
-              if (indicador == "humor" || indicador == "estabilidadeDeEmoções"){
+        for (var indicador in consulta){    
+
+            if (indicador != "registro" ){ 
+              console.log(consulta); 
+              if (indicador == "humor" || indicador == "estabilidadeDeEmoções")
                 soma += consulta[indicador]*3
-              }  
               else{
                 soma += consulta[indicador];
               }
               }
-              
           }
-          media.push(soma);
+          mediaConsulta.push(soma);
        });
         
        var legenda = [];
-       var i = 1;
-       media.forEach(valor => {
+       var i = 0;
+       mediaConsulta.forEach(valor => {
+         if (i==0)
+         legenda.push(" ")
+         else 
           legenda.push(i+"º consulta")
-          i++;
+         i++;
+         
        });
         const data = {
             labels: legenda,
             datasets: [
               {
                 
-                data: media,
+                data: mediaConsulta,
                 fill: false,
                 backgroundColor: 'rgba(45, 69, 97, 0.8)',
                 borderColor: 'rgba(45, 69, 97, 0.8)',
@@ -54,9 +59,7 @@ class GraficoEvolucaoPaciente extends Component {
               },
             ],
           };
-          
-         
-
+             
         return (
 
             <>
