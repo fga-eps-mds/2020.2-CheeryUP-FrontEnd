43c61589aab2d946d1792ec339c625c714650f25
Abraao Alves <abraao1231@gmail.com>
2021-05-06 11:58:37 -0300

    feat_add tamanho de grafico #140

--- src/components/Graficos/GraficoAvaliacaoConsulta.js
+++ src/components/Graficos/GraficoAvaliacaoConsulta.js
@@ -71,7 +71,7 @@ class GraficoAvaliaçãoMediaIndicadores extends Component {
                     <Pie data={data}
                         width={500}
                         height={500}
-                        options={{ maintainAspectRatio: false }} />
+                        options={{ maintainAspectRatio: false , }} />
                 </div>
 
             </>)
