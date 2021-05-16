8710aa436dc486f01e843d784baa23efe08904d0
juninhigh <nilvanperes@gmail.com>
2021-05-16 19:58:25 -0300

    add_DadosPsicPerfil

--- src/pages/PerfilPsicologo/PerfilPsicologo.js
+++ src/pages/PerfilPsicologo/PerfilPsicologo.js
@@ -1,35 +1,43 @@
 import React from "react"
 import "../../style/pages/PerfilPsicologo/PerfilPsicologo.css"
 import NavbarPerfil from "../../components/Navbar/NavbarPsicPerfil"
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const PerfilPsicologo = () => {
+    const { psic } = useSelector((state) => state);
     return (
-        <div className = "perfil-psicologo-container">
-            <NavbarPerfil/>
+      <div className="perfil-psicologo-container">
+        <NavbarPerfil />
 
-            <div className = "all">
-                <div className = "left">
-                    <div className = "data">
-                        <p><strong>Seus Dados</strong></p>
-                        <p>Nome do Psicologo</p>
-                        <p>Email</p>
-                        <p>Numero do CRP</p>
-                        <button>Alterar Dados</button>
-                    </div>
-                </div> 
-                <div className = "right">
-                    <div className = "rigth-content">
-                        <img src="/img/imgPerfilPsicologo.png" alt="imagem psicologo"/>
-                        <div className="buttons">
-                            <button>Lista de Pacientes</button>
-                            <button>Dashboards Gerais</button>
-                        </div>
-                    </div>
-                </div>
+        <div className="all">
+          <div className="left">
+            <div className="data">
+              <p>
+                <strong>Seus Dados</strong>
+              </p>
+              <p> Nome: {psic.name}</p>
+              <p>Email: {psic.user.email}</p>
+              <p>CRP: {psic.nCRP}</p>
+              <button>Alterar Dados</button>
             </div>
-            
+          </div>
+          <div className="right">
+            <div className="rigth-content">
+              <img src="/img/imgPerfilPsicologo.png" alt="imagem psicologo" />
+              <div className="buttons">
+                <Link to="/ListaPacientes">
+                  <button>Lista de Pacientes</button>
+                </Link>
+                <Link to="#">
+                  <button>Dashboards Gerais</button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default PerfilPsicologo;
