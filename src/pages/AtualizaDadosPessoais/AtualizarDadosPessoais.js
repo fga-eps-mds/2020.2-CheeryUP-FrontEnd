2f6f127f2ab0f56aba799842106263a3cd6903c4
antoniotoineto <netocaastro@gmail.com>
2021-05-19 12:22:15 -0300

    refact/PaginaAttDadosPsic

--- src/pages/AtualizaDadosPessoais/AtualizarDadosPessoais.js
+++ src/pages/AtualizaDadosPessoais/AtualizarDadosPessoais.js
@@ -9,92 +9,82 @@ import { useSelector } from "react-redux";
 const AttDadosPsico = ({ SubmitAttForm }) => {
   const { psic } = useSelector((state) => state);
   const { handleSubmit, handleChange, values } =
-    useFormAttContaPsic( SubmitAttForm );
+  useFormAttContaPsic( SubmitAttForm );
+  
   return (
-    <Fragment className="container-attPsico">
+    <Fragment>
       <NavbarPsic />
+      <div className="container-attPsico">
 
-      <div className="form-attPsico">
-        <div className="titulo-form-attPsico">
-          <h1 className="titulo-attPsico">Atualizar dados pessoais </h1>
-        </div>
-        <Form>
-          <div className="form-items-attPsico">
-            <div className="primeira-linha-attPsico">
-             {/*<Form.Input
-                placeholder= {psic.user.username}
-                label="Username"
-                required
-                fluid
-                onChange={handleChange}
-                name="username"
-                value={values.username}
-              />  */}
-              <Form.Input
-                placeholder= {psic.name}
-                label="Nome"
-                required
-                fluid
-                onChange={handleChange}
-                name="nome"
-                value={values.nome}
-              />
-              <Form.Input
-                required
-                onChange={handleChange}
-                value={values.email}
-                fluid
-                required
-                name="email"
-                label="Email"
-                placeholder= {psic.user.email}
-              />
-              <Form.Input
-                required
-                fluid
-                required
-                label="Número do CRP"
-                placeholder= {psic.nCRP}
-                onChange={handleChange}
-                value={values.nCRP}
-                name="nCRP"
-              />
-              <Form.Input
-                
-                label="Senha"
-                type="password"
-                required
-                fluid
-                onChange={handleChange}
-                name="senha"
-                value={values.senha}
-              />
-              <Form.Input
-                
-                label="Confirmar senha"
-                type="password"
-                required
-                fluid
-                onChange={handleChange}
-                name="senha2"
-                value={values.senha2}
-              />
-            </div>
-            <div className="quinta-linha-attPsico">
-              <Button onClick={handleSubmit}> Atualizar </Button>
-            </div>
+        <div className='container-titulo-form'>
+          <div className='titulo'>Atualizar dados pessoais</div>
+
+          <div className="form-attPsico">
+            <Form>
+              <div className="form-items-attPsico">
+                <div className="primeira-linha-attPsico">
+                  <Form.Input
+                    placeholder= {psic.name}
+                    label="Nome"
+                    required
+                    fluid
+                    onChange={handleChange}
+                    name="nome"
+                    value={values.nome}
+                  />
+                  <Form.Input
+                    required
+                    onChange={handleChange}
+                    value={values.email}
+                    fluid
+                    required
+                    name="email"
+                    label="Email"
+                    placeholder= {psic.user.email}
+                  />
+                  <Form.Input
+                    required
+                    fluid
+                    required
+                    label="Número do CRP"
+                    placeholder= {psic.nCRP}
+                    onChange={handleChange}
+                    value={values.nCRP}
+                    name="nCRP"
+                  />
+                  <Form.Input
+                    
+                    label="Senha"
+                    type="password"
+                    required
+                    fluid
+                    onChange={handleChange}
+                    name="senha"
+                    value={values.senha}
+                  />
+                  <Form.Input
+                    
+                    label="Confirmar senha"
+                    type="password"
+                    required
+                    fluid
+                    onChange={handleChange}
+                    name="senha2"
+                    value={values.senha2}
+                  />
+                </div>
+                <div className="quinta-linha-attPsico">
+                  <Button onClick={handleSubmit}> Atualizar </Button>
+                </div> 
+              </div>
+            </Form>
           </div>
-          <span className="form-input-attPsico">
-            <a href="/AtualizarSenha">
-              {/* <button type="button" className="default-buttonAlt">
-                Alterar Senha{" "}
-              </button> */}
-            </a>
-          </span>
-        </Form>
+        </div>
+        
       </div>
     </Fragment>
   );
 };
 
 export default AttDadosPsico;
+
