daaf16b5bf4e195d8748d6470bda1cbac530f72c
Nilvan Peres Jr <54211866+NilvanPeres@users.noreply.github.com>
2021-05-19 22:42:57 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://0.0.0.0:https://cheeryup-api.herokuapp.com/",
+  baseURL: "https://cheeryup-api.herokuapp.com/",
 
 });
 
